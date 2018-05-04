eb227adfd4ff84d8290b3779d882ca2378e31031
Brian Washington <bwash34@gmail.com>
2018-05-04 18:12:25 -0500

    Added animation for search link in the header.

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
 const articles = document.querySelectorAll(".article");
+const search = document.querySelector(".search");
+const input = document.querySelector(".input");
+
+let searchLoc = search.getBoundingClientRect();
+input.style["right"] = window.innerWidth - searchLoc.right - 15 + "px";
+
+window.addEventListener("resize", e => {
+  searchLoc = search.getBoundingClientRect();
+  input.style["right"] = window.innerWidth - searchLoc.right - 15 + "px";
+});
+
+search.addEventListener("click", e => {
+  e.preventDefault();
+  const link = search.querySelector("a");
+  const input = search.querySelector("input");
+
+  if (input.classList.contains("show")) {
+    return;
+  }
+
+  input.classList.add("show");
+  link.classList.add("hide");
+
+  input.addEventListener("transitionend", e => {
+    input.focus();
+  });
+
+  input.addEventListener("blur", e => {
+    input.classList.remove("show");
+    link.classList.remove("hide");
+  });
+});
 
 articles.forEach(article => {
   article.addEventListener("mouseover", e => {
