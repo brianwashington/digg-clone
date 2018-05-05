49f467af89fb464770a2a669b55dd0e6b3f86e32
Brian Washington <bwash34@gmail.com>
2018-05-04 21:58:38 -0500

    Added font awesome icons.

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
 const articles = document.querySelectorAll(".article");
 const search = document.querySelector(".search");
 const input = document.querySelector(".input");
-
-let searchLoc = search.getBoundingClientRect();
-input.style["right"] = window.innerWidth - searchLoc.right - 15 + "px";
+const link = search.querySelector("a");
 
 window.addEventListener("resize", e => {
-  searchLoc = search.getBoundingClientRect();
-  input.style["right"] = window.innerWidth - searchLoc.right - 15 + "px";
+  setInputPos();
 });
 
 search.addEventListener("click", e => {
   e.preventDefault();
-  const link = search.querySelector("a");
-  const input = search.querySelector("input");
-
   if (input.classList.contains("show")) {
     return;
   }
 
   input.classList.add("show");
   link.classList.add("hide");
+});
 
-  input.addEventListener("transitionend", e => {
-    input.focus();
-  });
+input.addEventListener("transitionend", e => {
+  input.focus();
+});
 
-  input.addEventListener("blur", e => {
-    input.classList.remove("show");
-    link.classList.remove("hide");
-  });
+input.addEventListener("blur", e => {
+  input.classList.remove("show");
+  link.classList.remove("hide");
 });
 
 articles.forEach(article => {
@@ -45,3 +39,8 @@ articles.forEach(article => {
       .forEach(link => link.classList.remove("underline"));
   });
 });
+
+function setInputPos() {
+  let searchLoc = search.getBoundingClientRect();
+  input.style["right"] = window.innerWidth - searchLoc.right + "px";
+}
